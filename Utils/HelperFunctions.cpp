3de02b1fe80faa9860e3389c947670f6f9ebd207
andrewDubyk <andrew.dubyk7@gmail.com>
2017-05-12 23:28:32 +0300

    moved functions into claas (at utils)

--- Utils/HelperFunctions.cpp
+++ Utils/HelperFunctions.cpp
@@ -1,8 +1,23 @@
-#include"HelperFunctions.h"
-
+#pragma once
+#include<vector>
+#include"../Entity/Advertisement.h"
+#include"../Utils/HelperFunctions.h"
 vector<Advertisement> HelperFunction::leaveAdvWithStatus(vector<Advertisement>& vect, bool statToLeave)
 {
-	return vector<Advertisement>();
+	{
+		for (size_t i = 0; i < vect.size(); i++)
+		{
+			while (vect[i].getStatus() != statToLeave)
+			{
+				vect.erase(vect.begin() + i);
+				if (i == vect.size())
+				{
+					break;
+				}
+			}
+		}
+		return vect;
+	}
 }
 
 string HelperFunction::timeOfCreating()
--- Utils/HelperFunctions.h
+++ Utils/HelperFunctions.h
@@ -1,35 +1,16 @@
-#pragma once
+#ifndef HelperFunction_HEADER
+#define HelperFunction_HEADER
+
+#include <iostream>
+using namespace std;
 #include <ctime>
 #include<string>
 
 class HelperFunction
 {
 public:
-	static	vector<Advertisement> leaveAdvWithStatus(vector<Advertisement>& vect, bool statToLeave)
-	{
-	for (size_t i = 0; i < vect.size(); i++)
-	{
-	while (vect[i].getStatus() != statToLeave)
-	{
-	vect.erase(vect.begin() + i);
-	if (i == vect.size())
-	{
-	break;
-	}
-	}
-	}
-	return vect;
-	}
-	
-	static string timeOfCreating()
-	{
-	char buffer[32];
-	time_t seconds = time(NULL);
-	tm timeinfo;
-	localtime_s(&timeinfo, &seconds);
-	asctime_s(buffer, 32, &timeinfo);
-	string time = (string)buffer;
-	return time;
-	}
-	
+	static	vector<Advertisement> leaveAdvWithStatus(vector<Advertisement>& vect, bool statToLeave);
+	static string timeOfCreating();	
 };
+
+#endif
