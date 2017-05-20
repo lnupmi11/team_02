a4d8c7c29ebb618e356da764cc698268bbd0b3ac
OlyaPastushchak <olyapastuschak@gmail.com>
2017-05-20 20:06:55 +0300

    some changes in advertisement output by rubrics

--- AOTH/ViewAllBeforeLogIn.h
+++ AOTH/ViewAllBeforeLogIn.h
@@ -42,79 +42,86 @@ void viewAllBeforeLogin()
 void viewAllBeforeLoginByRubric()
 {
 	system("cls");
-	cout << "\t\tHere you can find  advertisement \n";
 	vector<Advertisement> vectWithAllInformation;
 
 	DTOAdvertisement::getAllAdv(vectWithAllInformation);
 
 	HelperFunction::leaveAdvWithStatus(vectWithAllInformation, 1);
 
-	cout << "Choose the rubric , which you are looking for(enter number) :\n";
-	int action;
-	
-	for (int i = 0; i < 5; i++)
+	if (vectWithAllInformation.empty())
 	{
-		cout << i + 1 << " - " << getRubric(i) << "\n";
+		cout << "There aren't any advertisement yet";
 	}
-	
-	cin >> action;
-	
-	string rubric;
-	
-	switch (action - 1)
+	else
 	{
-    	case ForSale:
-    	{
-	    	rubric = "ForSale";
-	    	break;
-    	}
-    	case WantToBuy:
-    	{
-     		rubric = "WantToBuy";
-    		break;
-    	}
-    	case SomethingDisappeared:
-    	{
-    		rubric = "SomethingDisappeared";
-    		break;
-    	}
-    	case Meetings:
-    	{
-    		rubric = "Meetings";
-    		break;
-    	}
-    	case Advertising:
-    	{
-    		rubric = "Advertising";
-    		break;
-    	}
-   	}
+		cout << "\t\tHere you can find  advertisement \n";
+		cout << "Choose the rubric , which you are looking for(enter number) :\n";
+		int action;
 
-	system("cls");
-
-	for (int i = 0; i < 5; i++)
-	{
-		int counterPrintedRubrics = 0;
-		for (size_t j = 0; j < vectWithAllInformation.size(); j++)
+		for (int i = 0; i < 5; i++)
 		{
-			if (vectWithAllInformation[j].getRubric() == getRubric(i)&& rubric==getRubric(i))
-			{
-				if (counterPrintedRubrics == 0)
-				{
-					cout << "\n********************************************************************\n";
+			cout << i + 1 << " - " << getRubric(i) << "\n";
+		}
 
-					cout << "\nRubric:  ";
-					cout << vectWithAllInformation[j].getRubric() << "\n\n";
+		cin >> action;
+
+		string rubric;
+
+		switch (action - 1)
+		{
+		case ForSale:
+		{
+			rubric = "ForSale";
+			break;
+		}
+		case WantToBuy:
+		{
+			rubric = "WantToBuy";
+			break;
+		}
+		case SomethingDisappeared:
+		{
+			rubric = "SomethingDisappeared";
+			break;
+		}
+		case Meetings:
+		{
+			rubric = "Meetings";
+			break;
+		}
+		case Advertising:
+		{
+			rubric = "Advertising";
+			break;
+		}
+		}
+
+		system("cls");
+
+		for (int i = 0; i < 5; i++)
+		{
+			int counterPrintedRubrics = 0;
+			for (size_t j = 0; j < vectWithAllInformation.size(); j++)
+			{
+				if (vectWithAllInformation[j].getRubric() == getRubric(i) && rubric == getRubric(i))
+				{
+					if (counterPrintedRubrics == 0)
+					{
+						cout << "\n********************************************************************\n";
+
+						cout << "\nRubric:  ";
+						cout << vectWithAllInformation[j].getRubric() << "\n\n";
+					}
+					counterPrintedRubrics++;
+					cout << "User: " << vectWithAllInformation[j].getEmail() << endl;
+					cout << "\n";
+					cout << vectWithAllInformation[j];
+					cout << "\n";
 				}
-				counterPrintedRubrics++;
-				cout << "User: " << vectWithAllInformation[j].getEmail() << endl;
-				cout << "\n";
-				cout << vectWithAllInformation[j];
-				cout << "\n";
 			}
 		}
+		cout << "\n********************************************************************\n";
 	}
-	cout << "\n********************************************************************\n";
 	cout << "\n " << "\t\tPress any key to back to main menu ....";
 	system("pause>>null");
 
